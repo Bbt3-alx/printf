e2d3f150679a718231ea48bcefd9923ad7809fb6
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-08-21 11:59:03 -0700

    An update

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - produces output according to a format
  * @format: The character string
@@ -9,44 +8,41 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 	int i, len = 0;
-	char *str;
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(ap, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			len += write(STDOUT_FILENO, &format[i], 1);
+			len += write(1, &format[i], 1);
 			continue;
 		}
 		i++;
+		if (format[i] == '\0')
+		{
+			write(1, "%", 1);
+			return (-1);
+		}
 		switch (format[i])
 		{
-			case 's':
-				str = va_arg(ap, char *);
-				if (str == NULL)
-					len += write(STDOUT_FILENO, "(null)", 6);
-				else
-					len += print_s(str);
-					
-
-				break;
 			case 'c':
-				len += (char)print_c(va_arg(ap, int));
+				len += print_c((char)va_arg(ap, int));
+				break;
+			case 's':
+				len += print_s(va_arg(ap, char *));
 				break;
 			case 'd':
 			case 'i':
 				len += print_int(va_arg(ap, int));
 				break;
 			case '%':
-				len += write(STDOUT_FILENO, "%", 1);
+				len += write(1, "%", 1);
 				break;
 			default:
-				len += write(STDOUT_FILENO, "%%", 1);
-				len += write(STDOUT_FILENO, &format[i], 1);
+				len += write(1, "%%", 1);
+				len += write(1, &format[i], 1);
 				break;
 		}
 	}
